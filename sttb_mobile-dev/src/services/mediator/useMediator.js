3e278256ac8f0bac0c614a87e5e92c0a04743590
pafters <69425397+pafters@users.noreply.github.com>
2022-11-28 14:04:37 +0400

    Delete sttb_mobile-dev directory

--- sttb_mobile-dev/src/services/mediator/useMediator.js
+++
@@ -1,7 +0,0 @@
-import Mediator from './Mediator';
-
-export default function useMediator(SETTINGS) {
-    const { MESSAGES, MEDIATOR } = SETTINGS;
-    Object.keys(MESSAGES).forEach(key => MEDIATOR.EVENTS[key] = MESSAGES[key]);
-    return () => new Mediator(MEDIATOR);
-}
